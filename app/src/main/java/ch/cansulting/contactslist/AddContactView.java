6754538017708b104ece670e7ee9e8502e5c64d2
ademcan <ademcan@ademcan.net>
2019-02-02 01:25:16 +0100

    First commit

---
+++ app/src/main/java/ch/cansulting/contactslist/AddContactView.java
@@ -0,0 +1,13 @@
+package ch.cansulting.contactslist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddContactView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact_view);
+    }
+}
